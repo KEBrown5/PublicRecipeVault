31ad0d881b40559b7805da4ae1ba4aaae9793fc4
Kyle <kbrown5work@gmail.com>
2026-05-20 22:40:26 -0400

    Added redirection to login on empty url visit

--- TheRecipeVault/urls.py
+++ TheRecipeVault/urls.py
@@ -18,8 +18,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='/login/', permanent=False)),
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
     path('', include('users.urls')),
